632d72e65092fbaccb76f7cc6bea022f2ac0fa6b
CaioslppUO <37354152+CaioslppUO@users.noreply.github.com>
2020-02-26 16:02:05 -0300

    Add files via upload

--- src/raspberryRos/controller.py
+++ src/raspberryRos/controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-    Version: ROS 1.0.4
+    Version: ROS 1.0.5
     Date: 26/02/2020, 14:40
     Devs: Caio, Lucas, Levi
 
@@ -13,18 +13,36 @@
 
 import os
 import sys
+import rospy
+import time
+from std_msgs.msg import String
 from launcherVariables import LauncherVariables
+
+################################
+#----> Ros Initialization <----#
+################################
+
+os.system("roscore& ")
+time.sleep(5)
+os.system("clear && echo 'ROS has been successfully initialized'& ")
+
+##############################
+#----> Global Variables <----#
+##############################
+
+rospy.init_node('Controller', anonymous=True)
+pub = rospy.Publisher('Controller', String, queue_size=10)
 
 #######################
 #----> Main Loop <----#
 #######################
 
 def mainLoop():
+    global pub
     launcher = LauncherVariables()
     serverIp,enableUart,enableSensor,enableRelay,uartAmount = launcher.variableSeparator(sys.argv)
 
-    launchMsg = "roscore& "
-    launchMsg += "cd .. && python3 comunication/webServer.py " + serverIp + "& "
+    launchMsg = "cd .. && python3 comunication/webServer.py " + serverIp + "& "
     launchMsg += "cd .. && python3 comunication/comunication.py& "
     launchMsg += "cd .. && python3 modules/logs.py& "
     launchMsg += "cd .. && modules/controlModeDecider.py& "
@@ -37,6 +55,14 @@ def mainLoop():
         launchMsg += "cd .. && python3 modules/sensor.py& "
 
     os.system(launchMsg)
+
+    logPublish = str(serverIp) + "$"
+    logPublish += str(enableUart) + "$"
+    logPublish += str(enableSensor) + "$"
+    logPublish += str(enableRelay) + "$"
+    logPublish += str(uartAmount)
+    while not rospy.is_shutdown():
+        pub.publish(logPublish)
 
 if __name__ == "__main__":
     try:
