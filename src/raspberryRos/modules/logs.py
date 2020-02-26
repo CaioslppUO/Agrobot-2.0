913018ddb63127e5ddf69f2372d7aca2a00bbeb3
Lucas-Garavaglia <48389445+Lucas-Garavaglia@users.noreply.github.com>
2020-02-26 16:19:57 -0300

    Add files via upload

--- src/raspberryRos/modules/logs.py
+++ src/raspberryRos/modules/logs.py
@@ -8,20 +8,69 @@ import rospy
 import time
 from std_msgs.msg import String
 
-pubWebServer = rospy.Publisher('LogsWebServer', String, queue_size=10)
-pubComunication = rospy.Publisher('LogsComunication', String, queue_size=10)
+pubisherLog = rospy.Publisher('LogRobot', String, queue_size=10)
+
+ComunicationSpeed = None
+CommunicationSteer = None
+ComunicationLimit = None
+ComunicationPowerA = None
+ComunicationPowerB = None
+ComunicationPulverizer = None
+WebServerStrig = None
 
 def callbackComunication(data):
-    pubComunication.publish("Comunication info: " + str(data.data))
+    SplitComunication = str(data.data).split('$')
+    ComunicationSpeed = SplitComunication[0] 
+    ComunicationSteer = SplitComunication[1]
+    ComunicationLimit = SplitComunication[2]
+    ComunicationPowerA = SplitComunication[3]
+    ComunicationPowerB = SplitComunication[4]
+    ComunicationPulverizer = SplitComunication[5]
 
 def listenComunication():
     rospy.Subscriber("Comunication", String, callbackComunication) 
 
 def callbackWebServer(data):
-    pubWebServer.publish("WebServer info: " + str(data.data))
+    WebServerStrig = str(data.data)
 
 def listenWebServer():
-    rospy.Subscriber("WebServer", String, callbackWebServer)     
+    rospy.Subscriber("WebServer", String, callbackWebServer)   
+
+def printLog():
+    IndentationLog = "{0:<30}"
+    print("\x1b[2J\x1b[1;1H")
+    print(IndentationLog.format("Comunication variable"),end='|')
+    print(IndentationLog.format("Web Server variable"),end='|\n')
+
+    print(IndentationLog.format("Speed" + ComunicationSpeed),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    print(IndentationLog.format("Steer" + ComunicationSteer),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    print(IndentationLog.format("Limit" + ComunicationLimit),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    print(IndentationLog.format("PowerA" + ComunicationPowerA),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    print(IndentationLog.format("PowerB" + ComunicationPowerB),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    print(IndentationLog.format("Pulverizador" + ComunicationPulverizer),end='|')
+    print(IndentationLog.format(""),end='|\n')
+
+    
+
+def clearVariable():
+    ComunicationSpeed = None
+    CommunicationSteer = None
+    ComunicationLimit = None
+    ComunicationPowerA = None
+    ComunicationPowerB = None
+    ComunicationPulverizer = None
+    WebServerStrig = None
+
 
 #######################
 #----> Main Loop <----#
@@ -30,6 +79,8 @@ def listenWebServer():
 def main():
     listenComunication()
     listenWebServer()
+    printLog()
+    clearVariable()
     rospy.spin()
 
 try:
