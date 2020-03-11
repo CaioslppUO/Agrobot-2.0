2e2b36e6c42ec50331964299d9691b73454c9cb7
CaioslppUO <37354152+CaioslppUO@users.noreply.github.com>
2020-03-11 12:50:40 -0300

    Add files via upload

---
+++ src/raspberry/Controller.py
@@ -0,0 +1,128 @@
+"""
+    Version: 2.0.2
+    Date: 10/02/2020 , 13:52
+    Developers: Caio, Lucas, Levi
+"""
+
+#####################
+#----> Imports <----#
+#####################
+
+#Main imports
+from threading import Thread
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+import time
+
+#Libs imports
+from comunication.Comunication import Comunication
+from movement.Movement import Movement
+from complements.OutputMsgs import OutMsg
+from complements.Relay import Relay
+from comunication.LaunchInterface import LauncherInterface
+
+##############################
+#----> Global Variables <----#
+##############################
+
+uartAmount           = "None"
+serverIp             = "None"
+enableSensors        = "None"
+enableUart           = "None"
+enableRelays         = "None"
+
+##################################
+#----> Classes Declarations <----#
+##################################
+
+#LauncherInterface class
+launcher = LauncherInterface()
+
+###########################################
+#----> Launcher variables definition <----#
+###########################################
+
+enableSensors,enableUart,enableRelays,serverIp,uartAmount = launcher.getInputVariables()
+
+##################################
+#----> Classes Declarations <----#
+##################################
+
+#Communication class
+comunication = Comunication()
+
+#Movement class
+movement = Movement(enableSensors, enableUart, uartAmount)
+
+#OutMessages class
+outputMsg = OutMsg()
+
+#Relay class
+relays = Relay(enableRelays)
+
+########################
+#----> Web Server <----#
+########################
+
+server_address_httpd = (serverIp,8080)
+httpd = HTTPServer(server_address_httpd, comunication.RequestHandler_httpd)
+serverThread = Thread(target=httpd.serve_forever)
+serverThread.daemon = True #The server is closed when the program is closed
+serverThread.start()
+print('Server started')
+
+##############################
+#----> Robot management <----#
+##############################
+
+def controlRobot(msg,enableOutPutMessage,ignoreErrorMessage):
+    global relays,comunication
+    speed                = "None"
+    steer                = "None"
+    limit                = "None"
+    powerBoardA          = "None"
+    powerBoardB          = "None"
+    pulverizer           = "None"
+    speed,steer,limit,powerBoardA,powerBoardB,pulverizer = comunication.msgSeparator(msg,int(msg[0]),ignoreErrorMessage)
+
+    if(enableOutPutMessage == True):
+        #Writing in the screen the actual values 
+        outputMsg.printManualOutput(str(speed),str(steer),str(limit),str(powerBoardA),str(powerBoardB),str(pulverizer))
+
+    #Sending power signal to boards
+    relays.sendSignalToBoardOne(powerBoardA)
+    relays.sendSignalToBoardTwo(powerBoardB)
+
+    #Sending power signal to relay
+    relays.sendSignalToPulverizer(pulverizer)
+    
+    #Moving the robot
+    movement.setValues(speed,steer,limit)
+    movement.move()
+
+#######################
+#----> Main loop <----#
+#######################
+
+def mainLoop():
+    msgRecievedFromApp   = "None"
+    comunicationAttempts = 0
+    global comunication,serverIp
+    print('Server Ip:' + serverIp)
+    while True:
+        msgRecievedFromApp = comunication.getMsg()
+        if(msgRecievedFromApp):
+            controlRobot(msgRecievedFromApp,True,False)
+            time.sleep(0.1)
+            comunicationAttempts = 0
+        else:
+            controlRobot("0",False,True)
+            print('No message recieved from the app. Attempt: ' + str(comunicationAttempts))
+            comunicationAttempts = comunicationAttempts + 1
+            time.sleep(1.5)
+            
+if __name__ == "__main__":
+    try:
+        mainLoop()
+    except KeyboardInterrupt:
+        print('Program finalized')
