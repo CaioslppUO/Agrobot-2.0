01a9da693070303bea602c1332d415b9ec784c3f
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-05-02 15:27:19 -0300

    Novas funcionalidades Adicionadas. Clear e Marker

--- src/AppMaps/src/index.js
+++ src/AppMaps/src/index.js
@@ -1,22 +1,121 @@
 import React, { Component } from  'react';
-import { StyleSheet } from  'react-native';
-import MapView from  'react-native-maps';
+import { TouchableOpacity,Text,View } from  'react-native';
+import MapView, {Marker} from  'react-native-maps';
+import Style from './styles'
+import Geolocation from '@react-native-community/geolocation';
+
 
 export default class Mapa extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          markers: []
+        }
+        this.MarkPress = this.MarkPress.bind(this);
+        this.ClearPress = this.ClearPress.bind(this);
+        this.ShowPress = this.ShowPress.bind(this);
+      }
+
+      MarkPress(e) {
+        this.setState({
+          markers: [
+            ...this.state.markers,
+            {
+              coordinate: e.nativeEvent.coordinate,
+            }
+          ]
+        })
+        
+      }
+      ClearPress(e) {
+        this.state.markers.length = 0
+        this.setState({
+          markers: [
+            ...this.state.markers,
+          ]
+        })
+      }
+
+      ShowPress(e) {
+        console.log(this.state)
+      }
+      SetPress(e) {
+        // this.state.markers.includes(this.state.region)
+      }
+
+
+    state = {
+        region: null,
+    }
+    async componentDidMount(){
+        Geolocation.getCurrentPosition(
+            ({ coords: {latitude, longitude}}) => {
+                this.setState({
+                    region: {
+                        latitude, 
+                        longitude, 
+                        latitudeDelta: 0.009,
+                        longitudeDelta: 0.004,
+                    }
+                })
+            },
+            () => {},
+            {
+                timeout: 4000,
+                enableHighAccuracy: true,
+                maximumAge: 1000
+            }
+        )
+    }
     render() {
-        return <MapView style = {styles.map}
-            initialRegion = {{
-                latitude: 13.139238380834923,
-                longitude: 80.25188422300266,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}/>;
+        const {region} = this.state
+        return(
+        <>
+            <MapView 
+                style = {Style.map}
+                initialRegion = {region}
+                showsUserLocation
+                loadingEnabled
+                onPress={this.MarkPress}
+            >
+                {this.state.markers.map((marker) => {
+                    return (<Marker {...marker} />)
+                })}
+
+            </MapView>
+            <View style = {Style.ContainerButtons}>
+                <TouchableOpacity 
+                    style={Style.Buttons} 
+                    onPress={this.ClearPress}
+                >
+                    <Text style={Style.TextButton}>Clear</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={Style.Buttons}
+                    onPress={this.ShowPress}
+                >
+                    <Text style={Style.TextButton}>Show</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={Style.Buttons}
+                    onPress={this.SetPress}
+                >
+                    <Text style={Style.TextButton}>Set</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={Style.Buttons}
+                    onPress={() => {}}
+                >
+                    <Text style={Style.TextButton}>Start</Text>
+                </TouchableOpacity>
+
+            </View>
+        </>
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    map: {
-        height: 100,
-        flex: 1
-        }
-});
