07cf49756237f64aed110b94ef673b7e3a6d9b2b
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-05-08 15:39:23 -0300

    Algumas funcionalidades adicionadas

---
+++ src/config/instalador.sh
@@ -0,0 +1,153 @@
+function GerarWifi(){
+  echo "Enter network name: "
+  read network_name
+  echo "Enter password name"
+  read password_name
+  echo >> /etc/dhcpcd.conf "denyinterfaces wlan0"
+  echo >> /etc/network/interfaces "allow-hotplug wlan0
+    iface wlan0 inet static
+    address 192.168.1.2
+    netmask 255.255.255.0
+    network 192.168.1.1
+    broadcast 192.168.1.255"
+  echo >> /etc/hostapd/hostapd.conf "interface=wlan0
+    driver=nl80211
+    ssid=$network_name
+    hw_mode=g
+    channel=6
+    macaddr_acl=0
+    auth_algs=1
+    ignore_broadcast_ssid=0
+    wpa=2
+    wpa_passphrase=$password_name
+    wpa_key_mgmt=WPA-PSK
+    rsn_pairwise=CCMP"
+  echo >> /etc/default/hostapd 'DAEMON_CONF="/etc/hostapd/hostapd.conf"'
+  echo >> /etc/dnsmasq.conf "interface=wlan0
+    listen-address=192.168.1.2
+    bind-interfaces
+    server=8.8.8.8
+    domain-needed
+    bogus-priv
+    dhcp-range=192.168.1.120,192.168.1.254,12h"
+  echo >> /etc/sysctl.conf "net.ipv4.ip_forward=1"
+
+  sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+  sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+  sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+  echo "Raspberry turned into an Acces Point.
+  Ip: 192.168.1.2
+  NetWork name: $network_name
+  Password name: $password_name"
+}
+
+
+
+echo "Digite o numero equivalento a essa distro:
+1 - Ubuntu
+2 - Arch
+3 - Raspbian"
+read NumDistro
+
+#ATUALIZAR SISTEMA
+while true; do
+  if [ "$NumDistro" -eq 1 ];
+  then
+    clear
+    gerenciadorPacotes="apt-get install"
+    apt-get update && apt-get upgrade
+    clear
+    break;
+  elif [ "$NumDistro" -eq 2 ];
+  then
+    clear
+    gerenciadorPacotes="pacman -S"
+    pacman -Syyu
+    clear
+    break;
+  elif [ "$NumDistro" -eq 3 ];
+  then
+    clear
+    gerenciadorPacotes="apt-get install"
+    apt-get update && apt-get upgrade
+    clear
+    break;
+  else
+    echo "Distro não encontrada, favor digitar o numero novamente:"
+    read NumDistro
+  fi
+done
+
+# BAIXAR LIB DOS PINOS GPIO
+if [ "$NumDistro" -ne 2 ];
+  then
+    echo "Deseja instalar a lib dos GPIO?
+    1 - Sim
+    2 - Não"
+    read resposta
+    while true;do
+      if [ "$resposta" -eq 1 ];
+      then
+        sudo apt-get install rpi.gpio
+        clear
+        break;
+      elif [ "$resposta" -eq 2 ]
+      then
+        clear
+        break;
+      else
+        echo "Resposta inexistente, favor digitar novamente:"
+        read resposta
+    done
+
+# BAIXAR FONTE DO GIT
+echo "Deseja baixar o codigo fonte do Git?
+    1 - Sim
+    2 - Não"
+    read resposta
+    while true;do
+      if [ "$resposta" -eq 1 ];
+      then
+        gerenciadorPacotes git
+        git clone https://github.com/CaioslppUO/Agrobot-2.0
+        cd Agrobot-2.0
+        git checkout raspberry-ros
+        clear
+        break;
+      elif [ "$resposta" -eq 2 ]
+      then
+        clear
+        break;
+      else
+        echo "Resposta inexistente, favor digitar novamente:"
+        read resposta
+    done
+
+# ADICIONANDO USUARIO NO GRUPO PARA LER PORTA SERIAL
+adduser $USER uucp
+
+# BAIXANDO SSH
+$gerenciadorPacotes openssh*
+
+# GERAR WIFI
+echo "Deseja gerar um wifi com o rasp?
+    1 - Sim
+    2 - Não"
+    read resposta
+    while true;do
+      if [ "$resposta" -eq 1 ];
+      then
+        GerarWifi()
+        clear
+        break;
+      elif [ "$resposta" -eq 2 ]
+      then
+        clear
+        break;
+      else
+        echo "Resposta inexistente, favor digitar novamente:"
+        read resposta
+    done
