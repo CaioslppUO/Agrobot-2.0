d2ac2c837a222875da40062aace7f11d4d3daad3
CaioslppUO <caioslppuo@gmail.com>
2020-05-08 00:17:53 -0300

    Mudança no .gitignore.

--- src/raspberryRos/runnables/run_PC.sh
+++ src/raspberryRos/runnables/run_PC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../controller.py serverIp:179.106.209.229 enableUart:False enableSensor:False enableRelay:False uartAmount:0 commandObservers:1 enableFaceDetect:False
+python3 ../controller.py serverIp:192.168.1.11 enableUart:False enableSensor:False enableRelay:False uartAmount:0 commandObservers:1 enableFaceDetect:False
