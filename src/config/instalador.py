944d61855e7b7a00ebd2359a307a108f3eacc5d9
CaioslppUO <caioslppuo@gmail.com>
2020-05-10 08:16:51 -0300

    Refatoração no script de instalação 23.

--- src/config/instalador.py
+++ src/config/instalador.py
@@ -134,8 +134,6 @@ def installROS():
     run(command)
     command = "sudo apt install ros-melodic-desktop"
     run(command)
-    run("echo blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    time.sleep(3)
     run("clear")
     printOk("Instalação do ROS")
     configROS()
