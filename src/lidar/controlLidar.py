f0bbecf0f02e377aaea8a8606b3bf1a3d6206371
CaioslppUO <caioslppuo@gmail.com>
2020-05-10 03:04:07 -0300

    Refatoração da classe que lê os valores do lidar. Criação da classe que controla o robô em ambientes fechados.

--- src/lidar/controlLidar.py
+++ src/lidar/controlLidar.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import LaserScan
+from threading import Thread
+from std_msgs.msg import String
+
+vel = 100
+limite = 40
+direction = 0
+perto = 0.5
+medio = 2.0
+longe = 7.0
+
+pointDirection = split('$')
+def move():
+    print("B")
+
+def correctDirection():
+    print("c")
+
+def main():
+    print('a')
+
+def callback():
+    print("ola mundo")
+
+sub = rospy.Subscriber('/Lidar', String, callback)
--- src/lidar/lidarReader.py
+++ src/lidar/lidarReader.py
@@ -2,39 +2,44 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
 
 frange = [0,45,315,359]
 brange = [135,225]
 lrange = [44,134]
 rrange = [224,314]
 
+mf = 0
 mb = int((brange[0] + brange[1])/2)
 ml = int((lrange[0] + lrange[1])/2)
 mr = int((rrange[0] + rrange[1])/2)
+angleRange = 2
 
 def selectPoints(vet,range,centralPoint):
     i = 0
     selectedVet = []
-    selectedVet.add(vet[centralPoint])
+    selectedVet.append(vet[centralPoint])
     while(i < range):
-        selectPoints.add(vet[centralPoint+i])
-        selectPoints.add(vet[centralPoint-i])
+        selectedVet.append(vet[centralPoint+i])
+        selectedVet.append(vet[centralPoint-i])
         i = i+1
     return selectedVet
 
 def callback(msg):
-    print('Front: ' + str(getClosestObject(selectPoints(msg.ranges,7,0))))
-    print('Back: ' + str(getClosestObject(selectPoints(msg.ranges,7,mb))))
-    print('Left: ' + str(getClosestObject(selectPoints(msg.ranges,7,ml))))
-    print('Right: ' + str(getClosestObject(selectPoints(msg.ranges,7,mr))))
-
+    pubProcessedData.publish(str(getClosestObject(selectPoints(msg.ranges,angleRange,mf))) + '$'\
+    + str(getClosestObject(selectPoints(msg.ranges,angleRange,mb))) + '$'\
+    + str(getClosestObject(selectPoints(msg.ranges,angleRange,ml))) + '$'\
+    + str(getClosestObject(selectPoints(msg.ranges,angleRange,mr))))
 def getClosestObject(Vet):
     lowerValue = 100
     for testValue in Vet:
-        if (testValue < lowerValue):
-            lowerValue = testValue 
+        if(not isinstance(testValue, str)):
+            if (testValue < lowerValue):
+                lowerValue = testValue 
     return lowerValue
 
-rospy.init_node('lidar_values')
+
+rospy.init_node('lidar_values', anonymous=True)
+pubProcessedData = rospy.Publisher("Lidar", String,queue_size=10)
 sub = rospy.Subscriber('/scan', LaserScan, callback)
 rospy.spin()
