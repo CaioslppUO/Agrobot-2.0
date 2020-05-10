e48011080f0e207bcbcff68920a56f04fcf5751f
CaioslppUO <caioslppuo@gmail.com>
2020-05-10 08:16:34 -0300

    Refatoração no script de instalação 22.

--- src/config/instalador.py
+++ src/config/instalador.py
@@ -126,7 +126,8 @@ def configROS():
 
 def installROS():
     print(bcolors.OKGREEN + "Iniciando instalacao do ROS melodic" + bcolors.ENDC)
-    echoToFile("/etc/apt/sources.list.d/ros-latest.list","deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main",True)
+    command = "sudo sh -c 'echo 'deb http://packages.ros.org/ros/ubuntu bionic main' > /etc/apt/sources.list.d/ros-latest.list'"
+    run(command)
     command = "sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654"
     run(command)
     command = "sudo apt update"
