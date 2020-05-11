69c55301e4a9addbfcff6c5b4cb2b7c0cd35e25d
Lucas-Garavaglia <48389445+Lucas-Garavaglia@users.noreply.github.com>
2020-05-10 22:34:58 -0300

    Update controlLidar.py

--- src/lidar/controlLidar.py
+++ src/lidar/controlLidar.py
@@ -11,17 +11,44 @@ perto = 0.5
 medio = 2.0
 longe = 7.0
 
-pointDirection = split('$')
+frontPoint = 0
+backPoint = 0
+rightPoint = 0
+leftPoint = 0
+pointDirection = 0
+
+lastRightPoint = 0
+lastLeftPoint = 0
 def move():
-    print("B")
+    global frontPoint,backPoint,rightPoint,leftPoint
+    if( rightPoint < longe AND leftPoint < longe AND frontPoint < medio )
+        vel = 100
+        return True
+    else:
+        vel = 0
+        return False
 
 def correctDirection():
-    print("c")
+    global lastLeftPoint, lastRightPoint, rightPoint, leftPoint
+    if(lastLeftPoint - leftPoint > 0.20 AND rightPoint - lastRightPoint > 0.20)
+        direction+=10
+    elif(leftPoint - lastLeftPoint > 0.20 AND lastRightPoint - rightPoint > 0.20)
+        direction-=10
+    lastRightPoint = rightPoint
+    lastLeftPoint = leftPoint
 
 def main():
-    print('a')
+    sub = rospy.Subscriber('/Lidar', String, callback)
+    rospy.spin()
 
-def callback():
-    print("ola mundo")
+def callback(data):
+    global pointDirection,frontPoint,backPoint,rightPoint,leftPoint
+    pointDirection = str(data.data).split('$')
+    frontPoint = pointDirection[0]
+    backPoint = pointDirection[1]
+    rightPoint = pointDirection[2]
+    leftPoint = pointDirection[3]
+    if(move()):
+        correctDirection()
+    
 
-sub = rospy.Subscriber('/Lidar', String, callback)
