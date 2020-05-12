ef02a16ac5711a623340172273e9d7feac991cbf
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-05-12 14:55:57 -0300

    consertando cagada de logica do lucas

--- src/lidar/launchLidar.sh
+++ src/lidar/launchLidar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-source /home/labiot/catkin_ws/devel/setup.bash && roslaunch rplidar_ros rplidar.launch
+source /home/lucas/catkin_ws/devel/setup.bash && roslaunch rplidar_ros rplidar.launch
+
