1a944e9240670e856279c36bdf3095e046ade96f
CaioslppUO <caioslppuo@gmail.com>
2020-05-12 11:58:45 -0300

    Correções no fonte do robô.

--- src/raspberryRos/runnables/run_PC.sh
+++ src/raspberryRos/runnables/run_PC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /home/labiot/Agrobot-2.0/src/raspberryRos/controller.py serverIp:192.168.1.11 enableUart:False enableSensor:False enableRelay:False uartAmount:0 commandObservers:1 enableFaceDetect:False
+python3 /home/caioslpp/git/Agrobot-2.0/src/raspberryRos/controller.py serverIp:192.168.1.205 enableUart:False enableSensor:False enableRelay:False uartAmount:0 commandObservers:1 enableFaceDetect:False
