83f58a69f1b0e6a81f675a794ee0f46e971c1b18
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-05-19 18:18:16 -0300

    [v1.0.7][raspberry-ros-lidar]atualizaçãod o script

--- src/lidar/startLidar.sh
+++ src/lidar/startLidar.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# sleep 60
+sleep 60
 caminho="/home/labiot/Agrobot-2.0/src/lidar/"
 
 echo "executando lidar" 
 ${caminho}./launchLidar.sh &
-sleep 6
+sleep 10
 echo "executando lidarReader" 
 python3 ${caminho}lidarReader.py &
-sleep 3
+sleep 5
 echo "executando paramServer" 
 python3 ${caminho}paramServer.py &
-sleep 3
+sleep 5
 echo "executando readerParamsControllLidar" 
 python3 ${caminho}readerParamsControlLidar.py &
-sleep 3
+sleep 5
 echo "executando controlLidar" 
 python3 ${caminho}controlLidar.py &
