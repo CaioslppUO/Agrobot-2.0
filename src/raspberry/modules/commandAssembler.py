8a0e6ad2f5fe627bf74bdfa7d5e1a29de21857d4
CaioslppUO <caioslppuo@gmail.com>
2020-05-21 15:45:37 -0300

    [V 1.4.6][master] Adição da documentação do arquivo commandAssembler.py

--- src/raspberry/modules/commandAssembler.py
+++ src/raspberry/modules/commandAssembler.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Módulo que gerencia a montageme e distribuição dos comandos para os devidos dispositivos(hardware).
+"""
+
 #####################
 #----> Imports <----#
 #####################
@@ -17,39 +21,29 @@ rospy.init_node('CommandDecider', anonymous=True)
 #----> Classe Control Mode <----#
 #################################
 
+## Classe que gerência o recebimento dos comandos, separação e envio para os devidos dispositivos.
 class ControlMode():
+    ## Método que inicializa os tópicos em que serão publicados comandos
     def __init__(self):
         self.pubRelay = rospy.Publisher('Relay', String, queue_size=10)
         self.pubControlRobot = rospy.Publisher('ControlRobot', String, queue_size=10)
         self.pubCommandDecider = rospy.Publisher('CommandDecider', String, queue_size=10)
 
-    #Envia os comandos para os nodes do ROS
-    #Entrada: Velocidade,direção,limite,powerA,powerB e pulverizador
-    #Retorno: Nenhum
-    #Pré-condição: As variáveis devem estar corretas e dentro dos padrões para serem utilizadas
-    #Pós-condição: Cada nó responsável por realizar o comando por trás das variáveis recebe as variáveis necessárias
+    ## Método que envia os valores corretos para cada dispositivo.
     def sendComands(self,speed,steer,limit,powerA,powerB,pulverizer):
         self.pubControlRobot.publish(str(speed) + "$" + str(steer) + "$" + str(limit))
         self.pubRelay.publish("sendSignalToBoardOne:" + str(powerA))
         self.pubRelay.publish("sendSignalToBoardTwo:" + str(powerB))
         self.pubRelay.publish("sendSignalToPulverizer:" + str(pulverizer))
 
-    #Trata os dados recebidos pelo listenner
-    #Entrada: Dados recebidos pelo listenner
-    #Retorno: Nenhum
-    #Pŕe-condição: Nenhuma
-    #Pós-condição: Caso a conexão com a camada de comunicação esteja ativa, destina as variáveis para a função correta
+    ## Método que trata os comandos recebidos.
     def callbackComunication(self,data):
         if(str(data.data) != "No connection established."):
             cbAux = str(data.data).split("$")
             self.sendComands(int(cbAux[0]), int(cbAux[1]), int(cbAux[2]), int(cbAux[3]), int(cbAux[4]), int(cbAux[5]))
             self.pubCommandDecider.publish("manual")
     
-    #Escuta a camada de comunicação e executa a rotina necessária para tratar os dados
-    #Entrada: Nenhuma
-    #Retorno: Nenhum
-    #Pŕe-condição: Nenhuma
-    #Pós-condição: Ao escutar qualquer mensagem da camada de comunicação, a envia para a rotina correta
+    ## Método que escuta o tópico CommandPriorityDecider e chama a função que trata os comandos.
     def listenComunication(self):
         rospy.Subscriber("CommandPriorityDecider", String, self.callbackComunication)   
         rospy.spin()
