c9d1c22c9a3be3d456743c5ebf851878257aafb1
CaioslppUO <caioslppuo@gmail.com>
2020-05-26 16:56:50 -0300

    [V  0.1.0][raspberry-refactor] Padronização do arquivo relay.py  .

--- src/raspberry/modules/relay.py
+++ src/raspberry/modules/relay.py
@@ -4,25 +4,33 @@
 Módulo que gerencia o envio dos comandos para os relés.
 """
 
-#####################
-#----> Imports <----#
-#####################
+# ------------- #
+# -> Imports <- #
+# ------------- #
 
-import time
-import rospy
+import time,rospy
 from std_msgs.msg import String
 
-################################
-#----> Definições Globais <----#
-################################
+# ---------------- #
+# -> Constantes <- #
+# ---------------- #
 
-pubLog = rospy.Publisher('Log', String, queue_size=10)
-rospy.init_node("Relay", anonymous=True)
+const_pub_log = rospy.Publisher('log', String, queue_size=10)
+
+# ------------------- #
+# -> Configurações <- #
+# ------------------- #
+
+rospy.init_node("relay", anonymous=True)
+
+# -------------------------- #
+# -> Imports Condicionais <- #
+# -------------------------- #
 
 try: 
     import RPi.GPIO as GPIO
 except:
-    pubLog.publish("error$Warning$Could not include RPi.GPIO as GPIO in raspberry/modules/relay.py")
+    const_pub_log.publish("error$Warning$Could not include RPi.GPIO as GPIO in raspberry/modules/relay.py")
 
 try:
     ## Definição do modo do GPIO
@@ -30,16 +38,16 @@ try:
     ## Desabilitando os warnings do GPIO
     GPIO.setwarnings(False)
 except:
-    pubLog.publish('error$Warning$Could not set up GPIO configs in raspberry/modules/relay.py')
+    const_pub_log.publish('error$Warning$Could not set up GPIO configs in raspberry/modules/relay.py')
 
-#########################
-#----> Classe Relay <----#
-#########################
+# ------------- #
+# -> Classes <- #
+# ------------- #
 
 ## Classe que gerencia os relés.
 class Relay():
     ## Método que envia um sinal para o relé da placa A do hover board.
-    def sendSignalToBoardOne(self,signal):
+    def send_signal_to_board_one(self,signal):
         if(signal == 1):
             GPIO.setup(38, GPIO.OUT)
             GPIO.output(38, GPIO.HIGH)
@@ -53,7 +61,7 @@ class Relay():
             GPIO.output(38, GPIO.LOW)
 
     ## Método que envia um sinal para o relé da placa B do hover board.
-    def sendSignalToBoardTwo(self,signal):
+    def send_signal_to_board_two(self,signal):
         if(signal == 1):
             GPIO.setmode(GPIO.BOARD)
             GPIO.setwarnings(False)
@@ -69,7 +77,7 @@ class Relay():
             GPIO.output(40, GPIO.LOW)
 
     ## Método que envia um sinal para o relé do pulverizador/uv.
-    def sendSignalToPulverizer(self,signal):
+    def send_signal_to_pulverizer(self,signal):
         if(signal == 1):
             GPIO.setmode(GPIO.BOARD)
             GPIO.setwarnings(False)
@@ -83,35 +91,37 @@ class Relay():
             GPIO.output(35, GPIO.LOW)
 
     ## Método que trata os comandos recebidos pelo listener.
-    def callback(self,data):
-        cbAux = str(data.data).split(":")
-        if(cbAux[0] == "sendSignalToPulverizer"):
+    def callback(self,msg):
+        info = str(msg.data).split(":")
+        if(info[0] == "sendSignalToPulverizer"):
             try:
-                self.sendSignalToPulverizer(int(cbAux[1]))
+                self.send_signal_to_pulverizer(int(info[1]))
             except:
-                pubLog.publish('error$Warning$Could not send signal to pulverizer in raspberry/modules/relay.py')
-        elif(cbAux[0] == "sendSignalToBoardTwo"):
+                const_pub_log.publish('error$Warning$Could not send signal to pulverizer in raspberry/modules/relay.py')
+        elif(info[0] == "sendSignalToBoardTwo"):
             try:
-                self.sendSignalToBoardTwo(int(cbAux[1]))
+                self.send_signal_to_board_two(int(info[1]))
             except:
-                pubLog.publish('error$Warning$Could not send signal to board two in raspberry/modules/relay.py')
-        elif(cbAux[0] == "sendSignalToBoardOne"):
+                const_pub_log.publish('error$Warning$Could not send signal to board two in raspberry/modules/relay.py')
+        elif(info[0] == "sendSignalToBoardOne"):
             try:
-                self.sendSignalToBoardOne(int(cbAux[1]))
+                self.send_signal_to_board_one(int(info[1]))
             except:
-                pubLog.publish('error$Warning$Could not send signal to board one in raspberry/modules/relay.py')
+                const_pub_log.publish('error$Warning$Could not send signal to board one in raspberry/modules/relay.py')
     
     ## Método que escuta do tópico Relay e processa os comandos recebidos.
     def listener(self):
-        rospy.init_node('Relay', anonymous=True) 
-        rospy.Subscriber("Relay", String, self.callback)   
+        rospy.Subscriber("relay", String, self.callback)   
         rospy.spin()
 
-############################
-#----> Loop principal <----#
-############################
+# ------------------------- #
+# -> Execução de códigos <- #
+# ------------------------- #
 
 if __name__ == '__main__':
-    relay = Relay()
-    pubLog.publish('startedFile$Relay')
-    relay.listener()
+    try: 
+        relay = Relay()
+        const_pub_log.publish('startedFile$relay.py')
+        relay.listener()
+    except:
+        const_pub_log.publish("error$Fatal$Could not run relay.py.")
