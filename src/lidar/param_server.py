e64444d59dc1c06137ac21ef9b1fa69cdd55b155
CaioslppUO <caioslppuo@gmail.com>
2020-05-30 18:38:04 -0300

    [V 0.0.3][lidar-refactor] Refatoração do arquivo param_server.py .

--- src/lidar/param_server.py
+++ src/lidar/param_server.py
@@ -4,13 +4,11 @@
 Modulo que recebe os dados do App.
 """
 
-#####################
-#----> Imports <----#
-#####################
+# ------------- #
+# -> Imports <- #
+# ------------- #
 
-import rospy
-import sys
-
+import rospy,sys
 from std_msgs.msg import String
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
@@ -19,8 +17,8 @@ from threading import Thread
 # -> Constantes <- #
 # ---------------- #
 
-##Declaração do nó
-pub_web_server = rospy.Publisher('param_server', String, queue_size=10)
+const_pub_web_server = rospy.Publisher('param_server', String, queue_size=10)
+const_pub_log = rospy.Publisher("log", String, queue_size=10)
 
 # ------------------- #
 # -> Configurações <- #
@@ -32,35 +30,31 @@ rospy.init_node('param_server', anonymous=True)
 # -> Classes <- #
 # ------------- #
 
-
-##Classe Request Handler
+## Classe Request Handler.
 class RequestHandler_httpd(BaseHTTPRequestHandler):
-    ##Faz o manejo da mensagem quando existe uma conexão com o app
-    #Caso exista comunicação com app, a mensagem recebida é enviada para o tópico ROS
+    ## Faz o manejo da mensagem quando existe uma conexão com o app.
+    # Caso exista comunicação com app, a mensagem recebida é enviada para o tópico param_server.
     def do_GET(self):
-        global pub_web_server
         web_server_request = None
         web_server_request = self.requestline
         web_server_request = web_server_request[5 : int(len(web_server_request)-9)]
-        #Mensagem que vem do App de celular.
-        msg = str(web_server_request) 
-        pub_web_server.publish(str(msg))
+        msg = str(web_server_request) # Mensagem que vem do App de celular.
+        const_pub_web_server.publish(str(msg))
         msg = None
         return
 
-
-##Classe Web Server
+## Classe Web Server.
 class Param_server():
     def __init__(self):
         try:
-            self.serverIp = "192.168.1.121"
-            self.server_address_httpd = (self.serverIp,8082)
+            self.server_ip = "192.168.1.121"
+            self.server_address_httpd = (self.server_ip,8082)
             httpd = HTTPServer(self.server_address_httpd, RequestHandler_httpd)
             self.server_thread = Thread(target=httpd.serve_forever)
-            self.server_thread.daemon = True #O servidor é fechado ao fechar o programa
+            self.server_thread.daemon = True # O servidor é fechado ao fechar o programa.
             self.server_thread.start()
         except:
-            pass
+            const_pub_log.publish("error$Fatal$Could not run param_server.py.")
 
 # ------------------------ #
 # -> Execução de código <- #
@@ -68,9 +62,9 @@ class Param_server():
 ##Loop Principal  
 if __name__ == '__main__':
     try:
-        rospy.Publisher("Log",Strin,queue_size=10).publish("startedFile$Param_server")
-        Param_server = Param_server()
+        const_pub_log.publish("startedFile$param_server.py")
+        param_server = Param_server()
         while not rospy.is_shutdown():
             rospy.spin()
     except rospy.ROSInterruptException:
-        pass
+        const_pub_log.publish("error$Warning$param_server.py finalized.")
