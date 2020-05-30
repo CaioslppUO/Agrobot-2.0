5002b54dbe277dc3a7ab17e7fad5372e50bee84e
CaioslppUO <caioslppuo@gmail.com>
2020-05-30 18:42:00 -0300

    [V 0.0.4][lidar-refactor] Refatoração do arquivo get_angle.py .

--- src/lidar/get_angle.py
+++ src/lidar/get_angle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+
 """
 Modulo que pega os valores do sensor e publica
 """
-#####################
-#----> Imports <----#
-#####################
+
+# ------------- #
+# -> Imports <- #
+# ------------- #
+
 from __future__ import print_function
 import time, sys, signal, atexit, math
 from upm import pyupm_bmm150 as sensorObj
@@ -12,50 +15,58 @@ from upm import pyupm_bmm150 as sensorObj
 # ---------------- #
 # -> Constantes <- #
 # ---------------- #
-pub_angle = rospy.Publisher("angle", String,queue_size=10)
 
+const_pub_angle = rospy.Publisher("angle", String, queue_size=10)
+const_pub_log = rospy.Publisher("log", String, queue_size=10)
 
 # ------------------- #
 # -> Configurações <- #
 # ------------------- #
-rospy.init_node('angle', anonymous=True)
-rospy.Publisher("Log",Strin,queue_size=10).publish("startedFile$take_angle")
 
+rospy.init_node('angle', anonymous=True)
 
 # ------------- #
 # -> Funções <- #
 # ------------- #
+
+## Função que executa as rotinas de pegar os valores do acelerômetro e publicar no tópico angle.
 def main():
   sensor = sensorObj.BMM150(0, 0x13)
+
   def SIGINTHandler(signum, frame):
     raise SystemExit
+
   def exitHandler():
     print("Exiting")
     sys.exit(0)
+  
   atexit.register(exitHandler)
   signal.signal(signal.SIGINT, SIGINTHandler)
+  
   while (1):
     sensor.update()
     data = sensor.getMagnetometer()
-    xyHeading = math.atan2(data[0], data[1])
-    zxHeading = math.atan2(data[2], data[0])
-    heading = xyHeading
+    xy_heading = math.atan2(data[0], data[1])
+    zx_heading = math.atan2(data[2], data[0])
+    heading = xy_heading
+
     if heading < 0:
       heading += 2*math.pi
     if heading > 2*math.pi:
       heading -= 2*math.pi
-    headingDegrees = heading * 180/(math.pi);
-    pub_angle.publish(headingDegrees)
-    # print('heading(axis_Y point to): {0:.2f} degree'.format(headingDegrees))
+
+    heading_degrees = heading * 180/(math.pi);
+    const_pub_angle.publish(heading_degrees)
     time.sleep(.250)
 
 
 # ------------------------ #
 # -> Execução de código <- #
 # ------------------------ #
+
 if __name__ == '__main__':
   try:
+    const_pub_log.publish("startedFile$take_angle.py")
     main()
   except KeyboardInterrupt:
-    rospy.Publisher("Log",String,queue_size=10).publish("error$Warning$Program finalized")
-    print('Program finalized')
+    const_pub_log.publish("error$Warning$take_angle.py finalized.")
