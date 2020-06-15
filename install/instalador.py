93e7fd32a2d03a71e901de9a2d80f141ad6e0dc3
CaioslppUO <caioslppuo@gmail.com>
2020-06-15 19:29:39 -0300

    [V 0.0.5][instalador] Correções de nomeclatura.

--- install/instalador.py
+++ install/instalador.py
@@ -4,8 +4,8 @@ import os
 import time
 import subprocess
 
-wifi_name = "Agrobot4"
-wifi_password = "rapisbere"
+const_wifi_name = "Agrobot4"
+const_wifi_password = "rapisbere"
 const_repository_agrobot = "https://github.com/CaioslppUO/Agrobot-2.0"
 const_repository_lidar = "https://github.com/robopeak/rplidar_ros"
 const_user = "$USER"
@@ -18,13 +18,13 @@ lidar_task_check = False
 repo_task_check = False
 updt_task_check = False
 ports_task_check = False
-autoStartRobot = False
+auto_start_robot = False
 acces_task_check = False
 
 #Abre um arquivo novo, ou limpa o arquivo existente
-const_clean_file = "w"
+const_overwrite_file = "w"
 #Insere no arquivo, ou cria um arquivo
-const_insert_file = "a"
+const_append_to_file = "a"
 
 #Classe que guarda os padrões de cores usadas nos logs
 class bcolors:
@@ -80,7 +80,7 @@ def create_acess_point():
 
     file_content = list()
 
-    write_file("denyinterfaces wlan0\n","/etc/dhcpcd.conf",const_insert_file)
+    write_file("denyinterfaces wlan0\n","/etc/dhcpcd.conf",const_append_to_file)
 
     file_content.append("\nallow-hotplug wlan0\n")
     file_content.append("iface wlan0 inet static\n")
@@ -88,26 +88,26 @@ def create_acess_point():
     file_content.append("netmask 255.255.255.0\n")
     file_content.append("network 192.168.1.1\n")
     file_content.append("broadcast 192.168.1.255\n")
-    write_file(file_content,"/etc/network/interfaces",const_clean_file)
+    write_file(file_content,"/etc/network/interfaces",const_overwrite_file)
     file_content.clear()
     
     file_content.append("interface=wlan0\n")
     file_content.append("driver=nl80211\n")
-    file_content.append("ssid="+wifi_name+"\n")
+    file_content.append("ssid="+const_wifi_name+"\n")
     file_content.append("hw_mode=g\n")
     file_content.append("channel=6\n")
     file_content.append("macaddr_acl=0\n")
     file_content.append("auth_algs=1\n")
     file_content.append("ignore_broadcast_ssid=0\n")
     file_content.append("wpa=2\n")
-    file_content.append("wpa_passphrase="+wifi_password+"\n")
+    file_content.append("wpa_passphrase="+const_wifi_password+"\n")
     file_content.append("wpa_key_mgmt=WPA-PSK\n")
     file_content.append("rsn_pairwise=CCMP\n")
-    write_file(file_content,"/etc/hostapd/hostapd.conf",const_clean_file)
+    write_file(file_content,"/etc/hostapd/hostapd.conf",const_overwrite_file)
     file_content.clear()
 
     file_content.append("\nDAEMON_CONF='/etc/hostapd/hostapd.conf'\n")
-    write_file(file_content,"/etc/default/hostapd",const_insert_file)
+    write_file(file_content,"/etc/default/hostapd",const_append_to_file)
     file_content.clear()
 
     file_content.append("interface=wlan0\n")
@@ -117,11 +117,11 @@ def create_acess_point():
     file_content.append("domain-needed\n")
     file_content.append("bogus-priv\n")
     file_content.append("dhcp-range=192.168.1.120,192.168.1.254,12h\n")
-    write_file(file_content,"/etc/dnsmasq.conf",const_clean_file)
+    write_file(file_content,"/etc/dnsmasq.conf",const_overwrite_file)
     file_content.clear()
 
     file_content.append("\nnet.ipv4.ip_forward=1")
-    write_file(file_content,"/etc/sysctl.conf",const_insert_file)
+    write_file(file_content,"/etc/sysctl.conf",const_append_to_file)
     file_content.clear()
 
     command = "sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
@@ -135,7 +135,7 @@ def create_acess_point():
 
     file_content.append("\niptables-restore < /etc/iptables.ipv4.nat")
     file_content.append("\n/usr/sbin/hostapd /etc/hostapd/hostapd.conf")
-    write_file(file_content,"/usr/bin/resetssh.sh",const_insert_file)
+    write_file(file_content,"/usr/bin/resetssh.sh",const_append_to_file)
     file_content.clear()
 
     command = "sudo apt-get install ponte-utils"
@@ -176,10 +176,10 @@ def install_I2c():
     
     file_content.append("i2c-bcm2835")
     file_content.append("i2c-dev")
-    write_file(file_content,"/dev/modules",const_insert_file)
+    write_file(file_content,"/dev/modules",const_append_to_file)
     file_content.clear()
 
-    write_file("dtparam=i2c1=on","/boot/config.txt",const_insert_file)
+    write_file("dtparam=i2c1=on","/boot/config.txt",const_append_to_file)
     
     command = "sudo apt-get install -y python-smbus i2c-tools"
     run(command)
@@ -188,11 +188,11 @@ def install_I2c():
 
 #Da permissão ao usuario poder acessar as portas serial, e altera o nome das mesmas para o uso do ttl
 def serial_ports_configuration():
-    run("sudo usermod -a -G dialout " + user)
+    run("sudo usermod -a -G dialout " + const_user)
     file_content = list()
     file_content.append("SUBSYSTEM=='tty', KERNELS=='1-1.4:1.0', SYMLINK+='ttyUSB_CONVERSOR-0'\n")
     file_content.append("SUBSYSTEM=='tty', KERNELS=='1-1.5:1.0', SYMLINK+='ttyUSB_CONVERSOR-1'\n")
-    write_file(file_content,"/etc/udev/rules.d/99-usb-serial.rules",const_clean_file)
+    write_file(file_content,"/etc/udev/rules.d/99-usb-serial.rules",const_overwrite_file)
 
 #Instala e configura o ssh na maquina
 def to_set_up_ssh():
@@ -202,7 +202,7 @@ def to_set_up_ssh():
 
     file_content = list()
 
-    write_file("service ssh restart","/usr/bin/resetssh.sh",const_clean_file)
+    write_file("service ssh restart","/usr/bin/resetssh.sh",const_overwrite_file)
 
     command = "sudo chmod +x /usr/bin/resetssh.sh"
     run(command)
@@ -216,7 +216,7 @@ def to_set_up_ssh():
     file_content.append("\n")
     file_content.append("[Install]\n")
     file_content.append("WantedBy=multi-user.target\n")
-    write_file(file_content,"/etc/systemd/system/restartssh.service",const_clean_file)
+    write_file(file_content,"/etc/systemd/system/restartssh.service",const_overwrite_file)
     file_content.clear()
 
     command = "sudo chmod 644 /etc/systemd/system/restartssh.service"
@@ -260,8 +260,8 @@ def fix_bugs():
 def set_auto_start_robot_core():
     print(bcolors.OKGREEN + "Configurando a inicialização automática do robô" + bcolors.ENDC)
 
-    write_file("source /opt/ros/melodic/setup.bash && /home/labiot/Agrobot-2.0/src/raspberryRos/runnables/./run_ROBOT.sh","/usr/bin/autoStartRobotCore.sh",const_clean_file)
-    command = "sudo chmod +x /usr/bin/autoStartRobotCore.sh"
+    write_file("source /opt/ros/melodic/setup.bash && /home/labiot/Agrobot-2.0/src/raspberryRos/runnables/./run_ROBOT.sh","/usr/bin/auto_start_robotCore.sh",const_overwrite_file)
+    command = "sudo chmod +x /usr/bin/auto_start_robotCore.sh"
     run(command)
 
     file_content = list()
@@ -271,16 +271,16 @@ def set_auto_start_robot_core():
     file_content.append("")
     file_content.append("[Service]")
     file_content.append("Type=simple")
-    file_content.append("ExecStart=/bin/bash /usr/bin/autoStartRobotCore.sh")
+    file_content.append("ExecStart=/bin/bash /usr/bin/auto_start_robotCore.sh")
     file_content.append("")
     file_content.append("[Install]")
     file_content.append("WantedBy=multi-user.target")
-    write_file(file_content,"/usr/bin/autoStartRobotCore.sh",const_clean_file)
+    write_file(file_content,"/usr/bin/auto_start_robotCore.sh",const_overwrite_file)
     file_content.clear()
 
-    command = "sudo chmod 644 /etc/systemd/system/autoStartRobotCore.service"
+    command = "sudo chmod 644 /etc/systemd/system/auto_start_robotCore.service"
     run(command)
-    command = "sudo systemctl enable autoStartRobotCore"
+    command = "sudo systemctl enable auto_start_robotCore"
     run(command)
     run("clear")
     print_ok("Configuração da inicialização automática do robô")
@@ -297,7 +297,7 @@ def log():
     print('Lidar: ' + set_verified_color(lidar_task_check))
     print('AccessPoint: ' + set_verified_color(acces_task_check))
     print('UsbPortConfig: ' + set_verified_color(ports_task_check))
-    print("Iniciar Automáticamente o robô: " + set_verified_color(autoStartRobot))
+    print("Iniciar Automáticamente o robô: " + set_verified_color(auto_start_robot))
 
 #Menu do script
 def show_question(msg,function,errorMsg):
@@ -318,7 +318,7 @@ def show_question(msg,function,errorMsg):
         except:
             run("clear")
             print(bcolors.FAIL + errorMsg + bcolors.ENDC)
-            write_file("./log",errorMsg,const_insert_file)
+            write_file("./log",errorMsg,const_append_to_file)
             
             time.sleep(1)
             return False
@@ -327,10 +327,10 @@ def show_question(msg,function,errorMsg):
 #Função principal, faz a chamada de todas as funções do script, e seta os flags
 def main():
     run("clear")
-    write_file("./log","",const_clean_file)
+    write_file("./log","",const_overwrite_file)
     fix_bugs()
 
-    global gpio_task_check,i2c_task_check,ssh_task_check,lidar_task_check,repo_task_check,updt_task_check,ports_task_check,autoStartRobot,acces_task_check
+    global gpio_task_check,i2c_task_check,ssh_task_check,lidar_task_check,repo_task_check,updt_task_check,ports_task_check,auto_start_robot,acces_task_check
     serial_ports_configuration()
 
     ports_task_check = True
@@ -341,7 +341,7 @@ def main():
     i2c_task_check = show_question(bcolors.OKBLUE + 'Instalar e configurar o I2C?' + bcolors.ENDC,install_I2c,'Erro ao instalar o I2C')
     repo_task_check = show_question(bcolors.OKBLUE + 'Baixar o repositório do robô?' + bcolors.ENDC,download_repository,'Erro ao baixar o repositório remoto')
     acces_task_check = show_question(bcolors.OKBLUE + 'Configurar o RASP como access point?' + bcolors.ENDC,create_acess_point,'Erro ao configurar o AcessPoint')
-    autoStartRobot = show_question(bcolors.OKBLUE + "Configurando a inicialização automática do robô" + bcolors.ENDC,set_auto_start_robot_core,'Erro ao Configurar a inicialização automática do robô')
+    auto_start_robot = show_question(bcolors.OKBLUE + "Configurando a inicialização automática do robô" + bcolors.ENDC,set_auto_start_robot_core,'Erro ao Configurar a inicialização automática do robô')
     lidar_task_check = show_question(bcolors.OKBLUE + 'Instalar a biblioteca do RPLidar?***È NECESSÀRIO TER O ROS INSTALADO***' + bcolors.ENDC,install_lidar,'Erro ao configurar o AcessPoint')
     
     log()
