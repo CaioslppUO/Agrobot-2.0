2ab756738ccc663215f59fae87d3d61323b340f3
CaioslppUO <caioslppuo@gmail.com>
2020-07-22 20:53:07 -0300

    [app-refactor] Adição de mais proteções na tela de configurações manual.

--- src/coronaKiller/src/pages/Config/src.js
+++ src/coronaKiller/src/pages/Config/src.js
@@ -11,15 +11,21 @@ export default Src = {
             if(splittedIp[piece].length < 1 || splittedIp[piece].length > 3){
                 throw new Error("IP inválido.")
             }
+            if(typeof parseInt(splittedIp[piece]) != "number" || Number.isNaN(parseInt(parseInt(splittedIp[piece])))){
+                throw new Error("IP inválido.")
+            }
+            if(parseInt(splittedIp[piece]) < 0){
+                throw new Error("IP inválido.")
+            }
         }
     },
     checkPort: function(port){
-        if(typeof parseInt(port) != "number"){
+        if(typeof parseInt(port) != "number" || Number.isNaN(parseInt(port)) ||parseInt(port) < 0 ){
             throw new Error("Porta inválida.")
         }
     },
     checkDelay: function(delay){
-        if(typeof parseInt(delay) != "number"){
+        if(typeof parseInt(delay) != "number" || Number.isNaN(parseInt(delay)) || parseInt(delay) < 0){
             throw new Error("Delay inválido.")
         }
     }
