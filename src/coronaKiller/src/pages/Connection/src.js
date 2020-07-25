ef57f4c330e7f326b0a53813ea7d576ad11b2d50
CaioslppUO <caioslppuo@gmail.com>
2020-07-25 14:59:50 -0300

    [app-refactor] Envio de 500 comandos ao clicar em estabelecer conexão.

--- src/coronaKiller/src/pages/Connection/src.js
+++ src/coronaKiller/src/pages/Connection/src.js
@@ -1,9 +1,12 @@
 export default class Src {
     /** Tenta a comunicação com o servidor do roscore para acender o LED. */
     tryConnection() {
+      for(let i = 0; i < 500; i++){
         command =
           "http://" + global.roscoreServerIp + ":" + global.roscoreServerPort + "/" +
-          "speed$0*steer$0*limit$0*powerA$0*powerB$0*pulverize$0";
+          "speed$" +  (i%2 === 0? 0:1) +
+          "*steer$0*limit$0*powerA$0*powerB$0*pulverize$0";
         new WebSocket(command);
+      }
       }
 }
