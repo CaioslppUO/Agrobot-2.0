9022680b59a52e247cf27d69ef4164bd90a3adb5
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-10-13 10:43:33 -0300

    Correção do nome do pacote no instalador

--- install/install_xyz_sensor.sh
+++ install/install_xyz_sensor.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository ppa:mraa/mraa
 sudo apt-get update
-sudo apt-get install libmraa1 libmraa-dev mraa-tools python-mraa python3-mraa python-upm
+sudo apt-get install libmraa1 libmraa-dev mraa-tools python-mraa python3-mraa python3-upm
